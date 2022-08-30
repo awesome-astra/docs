357a17b200a8860da9e91107943a257a83fae292
Dieter Randolph <dieter.randolph@datastax.com>
2022-08-30 13:28:05 -0400

    adding NPM package

---
+++ index.js
@@ -0,0 +1,12 @@
+const context = require.context("./docs/tools/", true, /\.md$/);
+const all = [];
+context.keys().forEach((key) => {
+  if (key.includes("@riptano")) {
+    return;
+  }
+  const fileName = key.replace("./", "");
+  const resource = require(`./${fileName}`);
+  all[fileName] = resource;
+});
+
+export { all };
