3a0ed7639e9420f937c574e7c4f8faa76310ba5d
Dieter Randolph <dieter.randolph@datastax.com>
2022-09-02 10:41:28 -0400

    workflow mods

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const context = require.context("./", true, /\.md$/);
 const all = {};
-context.keys().forEach((key) => {
+context.keys().forEach((key, index) => {
   if (key.includes("@awesome-astra")) {
     return;
   }
   const fileName = key.replace("./", "");
   const resource = require(`./${fileName}`);
-  all[fileName] = resource;
+  all[index] = resource;
 });
 
 export { all };
