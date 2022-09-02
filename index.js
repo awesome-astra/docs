9f36bbf0be0949816d93bde1b8e68979a031758f
Dieter Randolph <dieter.randolph@datastax.com>
2022-09-02 12:49:55 -0400

    code cleanup

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const context = require.context("./", true, /\.md$/);
-const all = {};
-context.keys().forEach((key, index) => {
+const allMarkdown = [];
+context.keys().forEach((key) => {
   if (key.includes("@awesome-astra")) {
     return;
   }
   const fileName = key.replace("./", "");
   const resource = require(`./${fileName}`);
-  all[index] = resource;
+  allMarkdown.push(resource);
 });
 
-export { all };
+export { allMarkdown };
