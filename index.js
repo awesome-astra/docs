a46da369f61b75dd35ea60d471475b237504ba84
Dieter Randolph <dieter.randolph@datastax.com>
2022-09-06 16:54:34 -0400

    reverting polyfill

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-let context;
-try {
-  context = require.context("./", true, /\.md$/);
-} catch (error) {
-  const fs = require("fs");
-  const path = require("path");
-  require.context = (
-    base = "./",
-    scanSubDirectories = true,
-    regularExpression = /\.md$/
-  ) => {
-    const files = {};
-    function readDirectory(directory) {
-      fs.readdirSync(directory).forEach((file) => {
-        const fullPath = path.resolve(directory, file);
-        if (fs.statSync(fullPath).isDirectory()) {
-          if (scanSubDirectories) readDirectory(fullPath);
-          return;
-        }
-        if (!regularExpression.test(fullPath)) return;
-        files[fullPath] = true;
-      });
-    }
-    readDirectory(path.resolve(__dirname, base));
-    function Module(file) {
-      return require(file);
-    }
-    Module.keys = () => Object.keys(files);
-    return Module;
-  };
-  context = require.context("./", true, /\.md$/);
-}
-
-// const context = require.context("./", true, /\.md$/);
+const context = require.context("./", true, /\.md$/);
 const allMarkdown = [];
 context.keys().forEach((key) => {
   if (key.includes("@awesome-astra")) {
